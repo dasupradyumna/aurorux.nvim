2fdd0274a0a7ee5be7f25f63f2c84870cb5fddc0
Dasu Pradyumna <dasupradyumna@gmail.com>
2026-03-29 18:41:50 +0530

    feat: Update search highlights for better visibility

--- lua/aurorux/palette.lua
+++ lua/aurorux/palette.lua
@@ -22,6 +22,10 @@
 ---@type ColorPalette
 return {
     ----------------------------- GRAY HIGHLIGHTS -----------------------------
+    --- Hex range : 0 - f (15) & 4 segments [0-3] [4-7] [8-b] [c-f]
+    --- Maintain this difference between RGB channels for green-toned gray colors
+    ---     0 1 2 3 4 5 6 7 8 9 a b c d e f
+    ---     ( 130 ) ( 260 ) ( 390 ) ( 4c0 )
 
     normal  = { dark = '#191b18', light = '#ccd4c8' },  -- dark (1.5) light (12.5)
     gray1   = { dark = '#212320', light = '#626660' },  -- dark (2) light (6)
@@ -45,22 +49,3 @@ return {
 
     orange  = { bg = '#503408', fg = { dark = '#d47452', light = '#dbaa70' } },
 }
-
--- 1(statusline,inline-code) 2(curline,qfline) 3(winsep,folded,nontext,tablinesel)
--- palette.black   = { '#212320', '#313330', '#414340' }  -- 2 | 3 | 4
--- 1(conceal,lno,inlayhint) 2(statusline,comment) 3(modemsg)
--- palette.white   = { '#626660', '#939990', '#b3b9b0' }  -- 6 | 9 | 11
--- B(diffdelete) FD(spellbad,error) FL(operator,removed,inline-code,uncheckbox)
--- palette.red     = { '#32100d', '#bb4a4f', '#f0626a' }
--- B(diffadd) FD(property) FL(string,added,ok,checkbox)
--- palette.green   = { '#082a03', '#63a77a', '#96b68f' }
--- FD(spelllocal,hint,parameter) FL(dir,question,constant)
--- palette.blue    = { '#4f9dbc', '#78b0c4' }
--- B(visual) FD(title,type) FL(identifier,info)
--- palette.cyan    = { '#043430', '#67afa6', '#9cbdb8' }
--- FD(spellrare) FL(statement,link)
--- palette.magenta = { '#b363a9', '#c59bc1' }
--- BD(diffchange) BL(difftext) FD() FL(changed)
--- palette.yellow  = { '#2a2200', '#3a2e00', '#a9b670', '#c4c780' }
--- B(search) FD(spellcap,preproc,warn) FL(function)
--- palette.orange  = { '#503408', '#d47452', '#dbaa70' }
